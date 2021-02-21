e981861b07e839d08c033c2d76d766432b41a8b7
icon2341 <asharrahman2002@gmail.com>
2021-02-20 22:25:53 -0500

    JavaFX dep and interface

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,32 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class Main {
+//JavaFX import
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage)
+    {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/main.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
+
+
+
+
         try {
             URL url = new URL("https://api.wegmans.io/products/11914?api-version=2018-10-18");
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
