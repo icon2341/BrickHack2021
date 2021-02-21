15f0956afac56d4da5a9ae68581d9bc4b721652e
cdg8465 <cdg8465@rit.edu>
2021-02-20 20:50:24 -0500

    lemon

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Title of Window");
+
+        Scene scene = new Scene(root, 400, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        // HttpURLConnection huc = new HttpURLConnection();
+
+        URL url = null;
+        try {
+            url = new URL("https://api.wegmans.io/products/11914?api-version=2018-10-18");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        launch(args);
     }
 }
