590995a71bb78226537cb2a0b12d354b653a5c7f
cdg8465 <cdg8465@rit.edu>
2021-02-20 22:41:37 -0500

    JSON implementation is mostly done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -28,10 +29,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-
-
-
-
         try {
             URL url = new URL("https://api.wegmans.io/products/11914?api-version=2018-10-18");
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
@@ -50,8 +47,39 @@ public class Main extends Application {
 
             JSONObject jsonObject = new JSONObject(sb.toString());
             System.out.println("Product ID: " + jsonObject.get("sku"));
+            searchProduct("milk");
         } catch(IOException e) {
             e.printStackTrace();
         }
+    }
+    // https://api.wegmans.io/products/search?query={query}[&results][&page]
+    public static void searchProduct(String query) {
+        try {
+            URL url = new URL("https://api.wegmans.io/products/search?query=" + query + "&results=100&page=1&api-version=2018-10-18");
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            huc.setRequestProperty("Cache-Control", "no-cache");
+            huc.setRequestProperty("Subscription-Key", "98ca8bd853ca4621acd6bde0008bfea9");
+
+            huc.connect();
+            BufferedReader br = new BufferedReader(new InputStreamReader(huc.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            br.close();
+
+            JSONObject jsonObject = new JSONObject(sb.toString());
+            JSONArray results = (JSONArray) jsonObject.get("results");
+            JSONArray names = new JSONArray();
+            for(int i = 0; i < results.length(); i++) {
+                names.put(((JSONObject) results.get(i)).get("name"));
+            }
+            System.out.println(names.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
