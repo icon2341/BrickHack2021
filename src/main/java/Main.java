f95982c7645003d80d58bd4cb124081e4613a879
cdg8465 <cdg8465@rit.edu>
2021-02-21 03:34:59 -0500

    Restructured Files

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        JSONHandler test = new JSONHandler();
+        test.locateProduct(807487);
+    }
+}
